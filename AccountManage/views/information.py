dce8d5a558547e5cd58b58f210eb4e769f7ca822
wangchengqing <wangchengqing@yy.com>
2023-03-17 10:29:49 +0800

    准备添加我的信息

--- AccountManage/views/information.py
+++ AccountManage/views/information.py
@@ -6,3 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from AccountManage import models
 from AccountManage.utils.form import HDID
 from AccountManage.utils.pagination import Pagination
+
+
+def my_information(request):
+    pass
